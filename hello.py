0965ad991746eef16a056c6c4efc256203e5ea0c
jairot <jairotrad@gmail.com>
2015-10-17 13:31:23 -0300

    plumbing

--- hello.py
+++ hello.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('template.html')
+    return render_template('home.html')
+
+@app.route("/categories")
+def categories():
+    return render_template('categories.html')
+
+@app.route('/categories/<category>')
+def challenges(category=None):
+    return render_template("category.html", category=category)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
